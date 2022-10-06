27c77809a279cfa70873f60d188a535c85314c11
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-10-06 18:01:11 -0400

    changed Navbar button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,20 +3,20 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{background: '#212121'}}>
         <Toolbar>
-          <Typography variant="h3" component="div" sx={{ flexGrow: 1 , color: 'white'}}>
-            L
+          <Typography variant="h3" component="div" sx={{ flexGrow: 1}}>
+            L Z 
           </Typography>
-          <IconButton>
-            <MenuIcon sx={{color: 'white'}}/>
-          </IconButton>
+          <Button variant='contained' sx={{bgcolor: '#ab47bc'}}> Contact </Button>
         </Toolbar>
       </AppBar>
     </Box>
