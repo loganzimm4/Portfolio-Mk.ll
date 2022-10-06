327ec1a0e75ad72ab7a10066d1c10c0c8fba3568
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-10-06 17:44:36 -0400

    added about me section

--- src/Components/home.js
+++ src/Components/home.js
@@ -16,7 +16,6 @@ import HtmlSharpIcon from '@mui/icons-material/HtmlSharp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
-import reactLogo from './reactLogo.png'
 
 const Home = () => {
 
@@ -44,25 +43,45 @@ const Home = () => {
             >
               Logan Zimmerman
             </Typography>
+            
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Full-stack Developer recently graduated from The Flatiron School. 
+              This site is a continuing work-in-progress as I learn and create more projects to be a small 
+              representation of my capabilities. Thanks for visiting!
+            </Typography>
             <Box sx={{alignItems:'center', justifyContent:'center', display:'flex'}}>
                 <CssSharpIcon sx={{fontSize:'100px'}}/>
                 <JavascriptSharpIcon sx={{fontSize:'100px'}}/>
                 <HtmlSharpIcon sx={{fontSize:'100px'}}/>
             </Box>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Full-stack Developer with experience in JS, HTML, CSS, React, Ruby on Rails, etc. 
-              This site is a continuing work-in-progress as I learn and create more projects to be a small 
-              representation of my capabilities. Thanks for visiting!
-            </Typography>
+            
             <Stack
               sx={{ pt: 4 }}
               direction="row"
               spacing={2}
               justifyContent="center"
             >
-              <Button variant="contained" color='secondary'>About Me</Button>
-              <Button variant="contained" color='secondary'>Contact</Button>
+              {/* <Button variant="contained" color='secondary'>About Me</Button>
+              <Button variant="contained" color='secondary'>Contact</Button> */}
             </Stack>
+          </Container>
+        </Box>
+
+        <Box sx={{bgcolor: '#424242', width: '100%'}}>
+          <Typography variant='h2' sx={{color: '#ab47bc', display: 'flex', justifyContent: 'center', paddingBottom: '25px', paddingTop: '25px'}}> About Me </Typography>
+          <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: '100px', paddingTop: '25px'}}>
+              <Typography variant='h5' sx={{color: '#fff'}}> I am a Bootcamp graduate from The Flatiron School. 
+                  There, I learned the core principles and basic coding practices to best create applications that are useful 
+                  and easy to use. We were taught about JS, React, HTML, CSS, Ruby, Ruby on Rails, Github, SQL, and several 
+                  programs to utilize these skills, like VSCode, our terminals, Postman, and obviously Google! My journey into
+                  code was a long time in the making. My grandpa was fascinated with computers from their inception and always 
+                  had a curiosity for them, researching and building his own. After his passing, I began learning everything 
+                  I could about them. I was very into video games, and still am, and began morphing the two hobbies into one, 
+                  where I began building gaming pc's for myself and my friends almost all through high school and after learning 
+                  so much about computer hardware, there was only one obvious next step; computer programming. I began teaching 
+                  myself around September of 2021 and started my bootcamp in April of 2022. I am very excited to continue my pursuit 
+                  of coding and am currently working on several projects to add to this page. So welcome to my portfolio!
+              </Typography>
           </Container>
         </Box>
         
